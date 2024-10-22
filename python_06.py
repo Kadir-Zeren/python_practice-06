03d90bd0bbf9251bddd1c8d02840ed1f4d29a9bb
kadir-zeren <k4dzeren@gmail.com>
2024-10-22 10:31:14 +0300

    first commit

---
+++ python_06.py
@@ -0,0 +1,51 @@
+list_1 = ['h', 'a', 'p', 'p', 'y']
+word = 'happy'
+list_2 = list(word)
+print(list_1)
+print(list_2)
+
+country = ['USA', 'Brasil', 'UK', 'Germany', 'Turkey', 'New Zealand']
+print(country)
+
+string_1 = 'I quit smoking'
+new_list_1 = list(string_1) # we created multi element list
+print(new_list_1)
+new_list_2 = [string_1] # this is a single element list
+print(new_list_2)
+
+mixed_list = [11, 'Joseph', False, 3.14, None, [1, 2, 3]]
+
+my_list = ['Joseph', 'Clarusway', 2020]
+new_list1 = list(my_list) # what will be the output?
+new_list2 = [my_list]# what will be the output?
+print(new_list1)
+print(len(new_list1))# what is the length of the variable?
+print(new_list2)
+print(len(new_list2))
+
+listem = [1,2,3]
+len(listem)
+list(listem)
+
+a = [[1,2,3], ['ali' , 'ayse'], [True, False]]
+len(a)
+liste_a = [a]
+
+my_list1 = ["2023's perfect"]
+my_list2 = list("2023's perfect")
+print(my_list1)
+print(my_list2)
+
+city = ['Addis Ababa' , 'Istanbul', 'New York', 'Seoul' , 'Stockholm', 'Sydney' ]
+print(len(city))
+
+numbers = [1, 4, 7]
+numbers. append(9)
+numbers.append('9')
+print (numbers)
+
+empty_list = []
+empty_list.append(6666)
+empty_list. append('Multiverse')
+empty_list. append([0])
+print(empty_list)
